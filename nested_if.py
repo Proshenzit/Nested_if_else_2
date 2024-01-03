2828b31634d3850490cdc85a73f4e601558bdd88
Proshenzit Roy <37993972+Proshenzit@users.noreply.github.com>
2024-01-03 06:34:26 -0800

    Add files via upload

---
+++ nested_if.py
@@ -0,0 +1,9 @@
+number = int(input("enter your number: "))
+if  number>0:
+    if number==0:
+         print("the number is zero: ")
+    else:
+        print("positive ")
+else:
+        print("Negative")
+
